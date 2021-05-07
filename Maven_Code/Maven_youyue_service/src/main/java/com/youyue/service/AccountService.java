1559f63085f3ad343256407f53590aa797e2fadb
liergou <123321123@qq.com>
2021-05-07 10:17:35 +0800

    提交SSM项目

---
+++ Maven_Code/Maven_youyue_service/src/main/java/com/youyue/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.youyue.service;
+
+import com.youyue.domain.Account;
+
+import java.util.List;
+/*业务层*/
+public interface AccountService {
+
+    //查询所有
+    public List<Account> findAll();
+
+    //保存账户信息
+    public   void saveAccount(Account account);
+}
